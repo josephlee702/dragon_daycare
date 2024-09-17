ed37fab3b680a6a005845879b2fdb8170bca74ed
Joseph Lee <jhjlee702@gmail.com>
2024-09-17 14:26:24 -0600

    create skeleton for notes_controller.rb)

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,21 @@
+class NotesController
+  def index
+
+  end
+
+  def show(note_id)
+
+  end
+
+  def create(params)
+    note = Note.create()
+  end
+
+  def update()
+
+  end
+
+  def sign(note_id)
+    
+  end
+end
